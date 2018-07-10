c1d4d91737bfbe525b8746fff6c9d4b899423810
Jared Sorge <jared@jsorge.net>
2018-07-10 06:49:50 -0700

    Adding deploy script

---
+++ .tools/install_gitautodeploy.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+# verify that it's not installed
+
+# install
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:olipo186/git-auto-deploy
+sudo apt-get update
+sudo apt-get install git-auto-deploy
+
+# copy config
+sudo cp gitautodeploy.config.json /etc/git-auto-deploy.conf.json
+sudo chown git-auto-deploy /etc/git-auto-deploy.conf.json
+
+# copy ssh keys
+sudo cp ~/.ssh/id_rsa /etc/git-auto-deploy/.ssh/
+sudo chown -R git-auto-deploy:git-auto-deploy /etc/git-auto-deploy
+
+# make new user group that allows pulling
+sudo groupadd maverick
+
+# add the current user, git-auto-deploy, and root to the group
+sudo usermod -a -G maverick git-auto-deploy
+sudo usermod -a -G maverick $USER
+
+# make the group the owner of the working directory
+chgrp -R maverick .
+chmod -R g+swX .
+
+service git-auto-deploy start
+service git-auto-deploy status
--- Makefile
+++ Makefile
@@ -1,7 +1,21 @@
 .PHONY: serve
 serve:
 	docker-compose -f docker-compose.yml up --build
+	
+.PHOY: down
+down:
+	docker-compose down
 
 .PHONY: docker-logs
 docker-logs:
 	docker-compose logs maverick
+
+# Setup git auto deploy
+# https://github.com/olipo186/Git-Auto-Deploy
+.PHONY: install-autodeploy
+install-autodeploy:
+	./.tools/install_gitautodeploy.sh
+
+.PHONY: delete-autodeploy
+delete-autodeploy:
+	sudo apt-get remove git-auto-deploy
