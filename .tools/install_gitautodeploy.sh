ae93b8fdc0cb66fd74e78685bc47c6a3a1c3e4b6
Jared Sorge <jared@jsorge.net>
2018-07-18 22:41:34 -0700

    Updated autodeploy script

--- .tools/install_gitautodeploy.sh
+++ .tools/install_gitautodeploy.sh
@@ -1,19 +1,35 @@
 #! /usr/bin/env bash
+source .tools/parse_yaml.sh
 
 # verify that it's not installed
+if [ -d ".tools/vendor/Git-Auto-Deploy" ]; then
+  exit 0
+fi
 
+# setup variables
 wd=$(pwd)
+siteConfigPath="$wd/SiteConfig.yml"
+template="$wd/.tools/templates/gitautodeploy.config_template.json"
 
 # install
 cd .tools
-mkdir vendor
+
+if [ ! -d "vendor" ]; then
+	mkdir vendor
+fi
+
 cd vendor
 git clone https://github.com/olipo186/Git-Auto-Deploy.git
-sudo apt-get install python-pip
-sudo pip install -r requirements.txt
+cd Git-Auto-Deploy
+sudo -H apt-get install python-pip
+sudo -H pip install -r requirements.txt
 
 # copy config
-sudo cp $(wd)/gitautodeploy.config.json config.json
+eval $(parse_yaml $siteConfigPath)
+config=$(cat $template)
+config="${config/'{REPO_URL}'/$autodeploy_url}"
+config="${config/'{SITE_DIRECTORY}'/$autodeploy_directory}"
+echo $config > config.json
 
 # start the thing
 python -m gitautodeploy --config config.json
