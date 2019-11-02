458a91a5f964ba018244c690a9d33a8bf49e07a2
Jared Sorge <jared@jsorge.net>
2019-11-01 21:06:01 -0700

    Add publish script

---
+++ tools/PublishWorkingCopy.swift
@@ -0,0 +1,22 @@
+#!/usr/bin/swift sh
+
+//
+//  PublishWorkingCopy.swift
+//  
+//
+//  Created by Jared Sorge on 11/1/19.
+//
+
+import Foundation
+import ShellOut // @JohnSundell ~> 2.2
+
+do {
+    // commit files in a text bundle contained in the working copy
+    try shellOut(to: "git add *.textbundle/**")
+    try shellOut(to: .gitCommit(message: "New Post"))
+    try shellOut(to: .gitPush())
+
+    // ssh in to jsorge.net, at the directory of the website
+    // run git pull
+    try shellOut(to: "ssh jsorge@jsorge.net 'cd /var/www/jsorge.net/; git fetch && git pull'")
+}
