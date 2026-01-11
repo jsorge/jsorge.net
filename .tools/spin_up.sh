067bbed9241249bfa9d3e01f62916fff8810535e
Jared Sorge <jared@taphouse.io>
2026-01-11 09:10:02 -0800

    Remove SSL from the site
    It's proxied through Cloudflare now

--- .tools/spin_up.sh
+++ .tools/spin_up.sh
@@ -11,11 +11,10 @@ eval $(parse_yaml $siteConfigPath)
 config=$(cat $template)
 trimmedurl=$(echo "$url" | awk -F/ '{print $3}')
 config="${config/'{CONFIG_DOMAIN}'/$trimmedurl}"
-config="${config/'{CONFIG_EMAIL}'/$ssl_contactEmail}"
 config="${config/'{MAVERICK_VERSION}'/$maverickVersion}"
 config="${config/'{MAVERICK_REGISTRY}'/$MAVERICK_REGISTRY}"
 echo "$config" > $wd/.tools/docker-compose.yml
 
 docker pull "$MAVERICK_REGISTRY:$maverickVersion"
 
-docker-compose -f .tools/docker-compose.yml up --build
+docker-compose -f .tools/docker-compose.yml up --build -d
