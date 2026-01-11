32f8eecb7e73eea8ae5af792c54bd449510401da
Jared Sorge <jared@taphouse.io>
2026-01-11 10:36:53 -0800

    Update docker compose syntax

--- .tools/spin_up.sh
+++ .tools/spin_up.sh
@@ -4,7 +4,7 @@ source .tools/parse_yaml.sh
 # setup variables
 wd=$(pwd)
 siteConfigPath="$wd/SiteConfig.yml"
-template="$wd/.tools/templates/docker-compose_template.yml"
+template="$wd/.tools/templates/docker compose_template.yml"
 
 # copy config
 eval $(parse_yaml $siteConfigPath)
@@ -13,8 +13,8 @@ trimmedurl=$(echo "$url" | awk -F/ '{print $3}')
 config="${config/'{CONFIG_DOMAIN}'/$trimmedurl}"
 config="${config/'{MAVERICK_VERSION}'/$maverickVersion}"
 config="${config/'{MAVERICK_REGISTRY}'/$MAVERICK_REGISTRY}"
-echo "$config" > $wd/.tools/docker-compose.yml
+echo "$config" > $wd/.tools/docker compose.yml
 
 docker pull "$MAVERICK_REGISTRY:$maverickVersion"
 
-docker-compose -f .tools/docker-compose.yml up --build -d
+docker compose -f .tools/docker compose.yml up --build -d
