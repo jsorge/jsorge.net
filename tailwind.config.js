3a9cc80965f8356b07a060cad7bf2e765b91c1f4
Jared Sorge <jared@taphouse.io>
2026-07-14 22:21:58 -0700

    Styling improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
+            fontSize: '1.125rem',
+            lineHeight: '1.7',
+            p: {
+              'margin-top': '1em',
+              'margin-bottom': '1em',
+            },
+            ol: {
+              'margin-top': '1em',
+              'margin-bottom': '1em',
+            },
+            ul: {
+              'margin-top': '1em',
+              'margin-bottom': '1em',
+            },
             pre: null,
             code: null,
             'code::before': null,
@@ -19,14 +33,17 @@ module.exports = {
               color: '#e8804b',
             },
             h2: {
-              'margin-top': 0,
-              'margin-bottom': 0,
+              'font-size': '1.5rem',
+              'line-height': '1.3',
+              'margin-top': '1.75em',
+              'margin-bottom': '0.45em',
               color: '#e8804b',
-              a: {
-                link: {
-                  'text-decoration': 'none',
-                }
-              }
+            },
+            h3: {
+              'font-size': '1.25rem',
+              'line-height': '1.4',
+              'margin-top': '1.6em',
+              'margin-bottom': '0.45em',
             }
           }
         }
