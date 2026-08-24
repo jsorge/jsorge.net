6c935c56ff5e69d58cd0452a5abec61e81d66c65
Jared Sorge <jared@taphouse.io>
2026-08-23 20:41:16 -0700

    Update Maverick & prep for post broadcasting

--- mise/scripts/preview.sh
+++ mise/scripts/preview.sh
@@ -34,6 +34,7 @@ fi
 
 # Replace any previous preview container
 container delete --force "$CONTAINER_NAME" >/dev/null 2>&1 || true
+mkdir -p "$PWD/.maverick-secrets" "$PWD/.maverick-data"
 
 container run --detach --rm \
     --name "$CONTAINER_NAME" \
@@ -42,6 +43,8 @@ container run --detach --rm \
     --volume "$PWD/Public:/app/Public" \
     --volume "$PWD/Resources:/app/Resources" \
     --volume "$PWD/SiteConfig.yml:/app/SiteConfig.yml" \
+    --mount "type=bind,source=$PWD/.maverick-secrets,target=/run/secrets,readonly" \
+    --volume "$PWD/.maverick-data:/app/Data" \
     "$MAVERICK_REGISTRY:$version"
 
 # Wait for Maverick to start answering
